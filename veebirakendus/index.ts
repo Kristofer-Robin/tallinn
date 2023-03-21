8cc620dab25ff714620302062eb5253239c0806b
Kristofer-Robin <kristofer-robin.lai@voco.ee>
2023-03-21 10:05:21 +0200

    API päring teise rakendusse: Elering hindade kättesaamine

--- veebirakendus/index.ts
+++ veebirakendus/index.ts
@@ -5,6 +5,7 @@ import stringsController from "./controllers/strings";
 import productsController from "./controllers/products";
 import productListController from "./controllers/productlist";
 import parcelmachines from "./controllers/parcelmachines";
+import nordpool from "./controllers/nordpool";
 
 const app: Express = express();
 
@@ -21,6 +22,7 @@ app.use('/', stringsController);
 app.use('/', productsController);
 app.use('/', productListController);
 app.use('/', parcelmachines);
+app.use('/', nordpool);
 
 app.listen(3000,() => {
     console.log(`[server]: Server is running at http://localhost:3000`);
