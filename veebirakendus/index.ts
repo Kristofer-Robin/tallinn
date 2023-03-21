7fe6c753d2c71070333ebc219981e162bcf094ce
Kristofer-Robin <kristofer-robin.lai@voco.ee>
2023-03-21 09:57:02 +0200

    API päring teise rakendusse: pakiautomaatide kättesaamine

--- veebirakendus/index.ts
+++ veebirakendus/index.ts
@@ -1,20 +1,26 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 import stringsController from "./controllers/strings";
 import productsController from "./controllers/products";
-import productlistController from "./controllers/productlist"
-import cors from "cors";
-
+import productListController from "./controllers/productlist";
+import parcelmachines from "./controllers/parcelmachines";
 
 const app: Express = express();
+
+app.use(cors({
+    origin: ['http://localhost:3006']
+}));
+app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
 
-app.use(cors());
-app.use('/', productlistController);
-app.use('/', productsController);
 app.use('/', stringsController);
+app.use('/', productsController);
+app.use('/', productListController);
+app.use('/', parcelmachines);
 
 app.listen(3000,() => {
     console.log(`[server]: Server is running at http://localhost:3000`);
